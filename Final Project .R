ee3657a8c5f7d102a78de827eddf68e31ffd8f6d
maccwinter <matthew.winter1@louisiana.edu>
2019-12-03 20:40:18 -0600

    data frame made

--- Final Project .R
+++ Final Project .R
@@ -13,44 +13,35 @@ data <- read_xlsx('SFBVEG2013.xlsx')
 #indexing ----
 names(data)
 species <- unique(data$Species)
-named <- species[3:9]
+
 #subsetting ----
 
-jc1 <- subset(x = data, data$TransectID == 'Jaumea carnosa', select = c('Height', 'Distance', 'Density'))
+jc1 <- subset(x = data, data$TransectID == 'Jaumea carnosa', select = c('Canopy Height', 'Density'))
 jc1
 jc2 <- subset(x=data, data$Species == 'Jaumea carnosa', select = c('Canopy Height', '% Cover'))
 jc2
 
-#application of appropriate data storage structure? ---- 
-jc1 <- as.data.frame(jc1)
-jc2 <- as.data.frame(jc2)
-
-#merging data frames---- 
-
-jc1.0 <- merge(x=jc1, y =jc2)
-jc1.0
-
-jc1.0[,c(1,4)] <- NULL
-head(jc1.0)
 
 #ordering and summarizing ---- 
 library(tidyverse)
 
 names(data)
 
-
-data$Distance
 data$`% Cover` <- as.numeric(data$`% Cover`)
 
-sp.info <- data %>% group_by(Species) %>% 
-  summarise(avg.Cover = mean(`% Cover`, na.rm=T), Cover.stdev = sd(`% Cover`))
-sp.info
+sp.cover <- data %>% group_by(Species) %>% 
+  summarise(avg.Cover = mean(`% Cover`, na.rm=T), Cover.stdev = sd(`% Cover`, na.rm = T))
+sp.cover
 
+sp.Density <- data %>% group_by(Species) %>% 
+  summarise(avg.Density = mean(`Density`, na.rm=T), Density.stdev = sd(`Density`, na.rm = T))
+sp.Density
 
-
-
-
-
+sp.CD <- merge(x=sp.cover, y = sp.Density, by = 'Species' )
+sp.CD
+sp.CD[3,] 
+sp.CD <- sp.CD[-3,]
+sp.CD
 
 
 
